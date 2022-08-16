2df5d1ce9eada9882bdbf42a4e4988bd7d88f8bf
jackline chepngeno <chepngenojackline0@gmail.com>
2022-08-16 13:23:08 +0300

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length > 2 
+        array[0..-2].join(", ") + ", and " + array[-1]
+    elsif array.length == 2
+        array.join(" and ")
+        else
+            array[0]
+    end
 end
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
